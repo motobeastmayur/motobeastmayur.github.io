c64a6aae519ee3d79c4a33a4101401b2e90cfaf5
motobeastmayur <motobeastmayur@gmail.com>
2026-09-10 17:20:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    base: '/mayurbot.github.io/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
